ebb3a2287b73b8b9a538f2278b4c92816c682122
ffbrunoff1 <bruno@ntcbrasil.com.br>
2025-08-15 11:33:09 -0300

    Atualizar arquivos do projeto - 2025-08-15T14:33:09.860Z

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
       colors: {
+        background: '#f8fafc',
+        foreground: '#0f172a',
         primary: {
           50: '#f0f9ff',
           100: '#e0f2fe',
